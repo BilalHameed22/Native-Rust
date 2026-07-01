e8316ed9c810bbedbda91cfc0229ddc995f0f840
BilalHameed22 <bilalhameed@math.qau.edu.pk>
2026-07-01 18:29:43 +0500

    Lib file updated with description of the project

---
+++ hello_world/src/lib.rs
@@ -0,0 +1,4 @@
+// Goal: understand the absolute minimum shape of a native solana program
+//the entrypoint macro, the 'process_instruction" signature, logging, and 
+//how to iterate over the accounts passed into an instruction
+
